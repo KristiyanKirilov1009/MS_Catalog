b640fe471561774dae147a03acb4490d69279ae5
KristiyanKirilov1009 <k.vladimirov2004@gmail.com>
2022-03-19 19:37:48 +0200

    + comms

--- MS_Catalog/Interfaces/IUserDAO.cs
+++ MS_Catalog/Interfaces/IUserDAO.cs
@@ -7,11 +7,34 @@ namespace MS_Catalog.Interfaces
 {
     public interface IUserDAO
     {
-        //All the methods UserDAO implements. 
-
+         
+        /// <summary>
+        /// Gets user by id.
+        /// </summary>
+        /// <param name="userId">User's id.</param>
+        /// <returns>Returns user.</returns>
         User GetUserById(int userId);
+
+
+        /// <summary>
+        /// Gets user by username.
+        /// </summary>
+        /// <param name="username">User's username.</param>
+        /// <returns>Returns user.</returns>
         User GetUserByUsername(string username);
+
+        /// <summary>
+        /// Gets user by username and password.
+        /// </summary>
+        /// <param name="username">User's username.</param>
+        /// <param name="password">User's password.</param>
+        /// <returns>Returns user.</returns>
         User GetUserByUsernameAndPass(string username, string password);
+
+        /// <summary>
+        /// Makes registration
+        /// </summary>
+        /// <param name="user">User.</param>
         void Registration(User user);
     }
 }
